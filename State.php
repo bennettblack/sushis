6982bb9caebe9f5e327fe9e984973c5d0a02adc0
Bennett <bennett@sent.com>
2024-01-12 11:02:40 -0600

    Initial Commit

---
+++ State.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Models\Sushi;
+
+use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
+
+class State extends Model
+{
+    use Sushi;
+
+    protected $rows = [
+        [
+            'name' => 'Alabama',
+            'abbreviation' => 'AL'
+        ],	
+        [
+            'name' => 'Alaska',
+            'abbreviation' => 'AK'
+        ],	
+        [
+            'name' => 'Arizona',
+            'abbreviation' => 'AZ'
+        ],	
+        [
+            'name' => 'Arkansas',
+            'abbreviation' => 'AR'
+        ],	
+        [
+            'name' => 'California',
+            'abbreviation' => 'CA'
+        ],	
+        [
+            'name' => 'Canal Zone',
+            'abbreviation' => 'CZ'
+        ],	
+        [
+            'name' => 'Colorado',
+            'abbreviation' => 'CO'
+        ],
+        [
+            'name' => 'Connecticut',
+            'abbreviation' => 'CT'
+        ],	
+        [
+            'name' => 'Delaware',
+            'abbreviation' => 'DE'
+        ],	
+        [
+            'name' => 'Florida',
+            'abbreviation' => 'DL'
+        ],
+        [
+            'name' => 'Georgia',
+            'abbreviation' => 'GA'
+        ],	
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'HI'
+        ],
+        [
+            'name' => 'Idaho',
+            'abbreviation' => 'ID'
+        ],
+        [
+            'name' => 'Illinois',
+            'abbreviation' => 'IL'
+        ],
+        [
+            'name' => 'Indiana',
+            'abbreviation' => 'IN'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'IA'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'KS'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'KY'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'LA'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'ME'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'MD'
+        ],
+        [
+            'name' => 'Hawaii',
+            'abbreviation' => 'MA'
+        ],
+        [
+            'name' => 'Michigan',
+            'abbreviation' => 'MI'
+        ],	
+        [
+            'name' => 'Minnesota',
+            'abbreviation' => 'MN'
+        ],	
+        [
+            'name' => 'Mississippi',
+            'abbreviation' => 'MS'
+        ],	
+        [
+            'name' => 'Missouri',
+            'abbreviation' => 'MO'
+        ],	
+        [
+            'name' => 'Montana',
+            'abbreviation' => 'MT'
+        ],	
+        [
+            'name' => 'Nebraska',
+            'abbreviation' => 'NE'
+        ],	
+        [
+            'name' => 'Nevada',
+            'abbreviation' => 'NV'
+        ],	
+        [
+            'name' => 'New Hampshire',
+            'abbreviation' => 'NH'
+        ],
+        [
+            'name' => 'New Jersey',
+            'abbreviation' => 'NJ'
+        ],
+        [
+            'name' => 'New Mexico',
+            'abbreviation' => 'NM'
+        ],
+        [
+            'name' => 'New York',
+            'abbreviation' => 'NY'
+        ],
+        [
+            'name' => 'North Carolina',
+            'abbreviation' => 'NC'
+        ],	
+        [
+            'name' => 'North Dakota',
+            'abbreviation' => 'ND'
+        ],	
+        [
+            'name' => 'Ohio',
+            'abbreviation' => 'OH'
+        ],	
+        [
+            'name' => 'Oklahoma',
+            'abbreviation' => 'OK'
+        ],	
+        [
+            'name' => 'Oregon',
+            'abbreviation' => 'OR'
+        ],	
+        [
+            'name' => 'Pennsylvania',
+            'abbreviation' => 'PA'
+        ],	
+        [
+            'name' => 'Rhode Island',
+            'abbreviation' => 'RI'
+        ],	
+        [
+            'name' => 'South Carolina',
+            'abbreviation' => 'SC'
+        ],	
+        [
+            'name' => 'South Dakota',
+            'abbreviation' => 'SD'
+        ],	
+        [
+            'name' => 'Tennessee',
+            'abbreviation' => 'TN'
+        ],	
+        [
+            'name' => 'Texas',
+            'abbreviation' => 'TX'
+        ],	
+        [
+            'name' => 'Utah',
+            'abbreviation' => 'UT'
+        ],	
+        [
+            'name' => 'Vermont',
+            'abbreviation' => 'VT'
+        ],	
+        [
+            'name' => 'Virginia',
+            'abbreviation' => 'VA'
+        ],	
+        [
+            'name' => 'Washington',
+            'abbreviation' => 'WA'
+        ],	
+        [
+            'name' => 'West Virginia',
+            'abbreviation' => 'WV'
+        ],	
+        [
+            'name' => 'Wisconsin',
+            'abbreviation' => 'WI'
+        ],	
+        [
+            'name' => 'Wyoming',
+            'abbreviation' => 'WY'
+        ],	
+    ];
+}
