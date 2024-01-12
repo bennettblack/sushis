c278bb9fc32954024b9e1277ed4a1a6acc23d5ea
Bennett <bennett@sent.com>
2024-01-12 11:41:45 -0600

    Separate Language Abbr's

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![Alt text](assets/elephant_sushi.png)
 # Sushis
-Sushi models for common use-cases. 
+
+[Sushi](https://github.com/calebporzio/sushi) models for common use-cases. 
+
 - `State.php` All 50 States
 - `Language.php` 100+ most used languages in the world
--- models/Language.php
+++ models/Language.php
@@ -10,112 +10,433 @@ class Language extends Model
     use Sushi;
 
     protected $rows = [
-        ['name' => 'AKAN [TWS]'],
-        ['name' => 'AMHARIC [AMH]'],
-        ['name' => 'ARABIC, ALGERIAN SPOKEN [ARQ]'],
-        ['name' => 'ARABIC, EGYPTIAN SPOKEN [ARZ]'],
-        ['name' => 'ARABIC, MESOPOTAMIAN SPOKEN [ACM]'],
-        ['name' => 'ARABIC, MOROCCAN SPOKEN [ARY]'],
-        ['name' => 'ARABIC, NAJDI SPOKEN [ARS]'],
-        ['name' => 'ARABIC, NORTH LEVANTINE SPOKEN [APC]'],
-        ['name' => 'ARABIC, SAIDI SPOKEN [AEC]'],
-        ['name' => 'ARABIC, SANAANI SPOKEN [AYN]'],
-        ['name' => 'ARABIC, SUDANESE SPOKEN [APD]'],
-        ['name' => 'ARABIC, TUNISIAN SPOKEN [AEB]'],
-        ['name' => 'ASSAMESE [ASM]'],
-        ['name' => 'AWADHI [AWD]'],
-        ['name' => 'AZERBAIJANI, NORTH [AZE]'],
-        ['name' => 'AZERBAIJANI, SOUTH [AZB]'],
-        ['name' => 'BELARUSAN [RUW]'],
-        ['name' => 'BENGALI [BNG]'],
-        ['name' => 'BHOJPURI [BHJ]'],
-        ['name' => 'BULGARIAN [BLG]'],
-        ['name' => 'BURMESE [BMS]'],
-        ['name' => 'CEBUANO [CEB]'],
-        ['name' => 'CHHATTISGARHI [HNE]'],
-        ['name' => 'CHINESE, GAN [KNN]'],
-        ['name' => 'CHINESE, HAKKA [HAK]'],
-        ['name' => 'CHINESE, JINYU [CJY]'],
-        ['name' => 'CHINESE, MANDARIN [CHN]'],
-        ['name' => 'CHINESE, MIN BEI [MNP]'],
-        ['name' => 'CHINESE, MIN NAN [CFR]'],
-        ['name' => 'CHINESE, WU [WUU]'],
-        ['name' => 'CHINESE, XIANG [HSN]'],
-        ['name' => 'CHINESE, YUE [YUH]'],
-        ['name' => 'CHITTAGONIAN [CIT]'],
-        ['name' => 'CZECH [CZC]'],
-        ['name' => 'DECCAN [DCC]'],
-        ['name' => 'DUTCH [DUT]'],
-        ['name' => 'ENGLISH [ENG]'],
-        ['name' => 'FARSI, EASTERN [PRS]'],
-        ['name' => 'FARSI, WESTERN [PES]'],
-        ['name' => 'FRENCH [FRN]'],
-        ['name' => 'FULFULDE, NIGERIAN [FUV]'],
-        ['name' => 'GERMAN, STANDARD [GER]'],
-        ['name' => 'GREEK [GRK]'],
-        ['name' => 'GUJARATI [GJR]'],
-        ['name' => 'HAITIAN CREOLE FRENCH [HAT]'],
-        ['name' => 'HARYANVI [BGC]'],
-        ['name' => 'HAUSA [HUA]'],
-        ['name' => 'HILIGAYNON [HIL]'],
-        ['name' => 'HINDI [HND]'],
-        ['name' => 'HUNGARIAN [HNG]'],
-        ['name' => 'IGBO [IGR]'],
-        ['name' => 'ILOCANO [ILO]'],
-        ['name' => 'INDONESIAN [INZ]'],
-        ['name' => 'ITALIAN [ITN]'],
-        ['name' => 'JAPANESE [JPN]'],
-        ['name' => 'JAVANESE [JAN]'],
-        ['name' => 'KANNADA [KJV]'],
-        ['name' => 'KAZAKH [KAZ]'],
-        ['name' => 'KHMER, CENTRAL [KMR]'],
-        ['name' => 'KOREAN [KKN]'],
-        ['name' => 'KURMANJI [KUR]'],
-        ['name' => 'LOMBARD [LMO]'],
-        ['name' => 'MADURA [MHJ]'],
-        ['name' => 'MAGAHI [MQM]'],
-        ['name' => 'MAITHILI [MKP]'],
-        ['name' => 'MALAGASY [MEX]'],
-        ['name' => 'MALAY [MLI]'],
-        ['name' => 'MALAYALAM [MJS]'],
-        ['name' => 'MARATHI [MRT]'],
-        ['name' => 'MARWARI [MKD]'],
-        ['name' => 'NAPOLETANO-CALABRESE [NPL]'],
-        ['name' => 'NEPALI [NEP]'],
-        ['name' => 'ORIYA [ORY]'],
-        ['name' => 'OROMO, WEST-CENTRAL [GAZ]'],
-        ['name' => 'PANJABI, EASTERN [PNJ]'],
-        ['name' => 'PANJABI, WESTERN [PNB]'],
-        ['name' => 'PASHTO, NORTHERN [PBU]'],
-        ['name' => 'PASHTO, SOUTHERN [PBT]'],
-        ['name' => 'POLISH [PQL]'],
-        ['name' => 'PORTUGUESE [POR]'],
-        ['name' => 'ROMANIAN [RUM]'],
-        ['name' => 'RUSSIAN [RUS]'],
-        ['name' => 'RWANDA [RUA]'],
-        ['name' => 'SARAIKI [SKR]'],
-        ['name' => 'SERBO-CROATIAN [SRC]'],
-        ['name' => 'SHONA [SHD]'],
-        ['name' => 'SINDHI [SND]'],
-        ['name' => 'SINHALA [SNH]'],
-        ['name' => 'SOMALI [SOM]'],
-        ['name' => 'SPANISH [SPN]'],
-        ['name' => 'SUNDA [SUO]'],
-        ['name' => 'SWEDISH [SWD]'],
-        ['name' => 'TAGALOG [TGL]'],
-        ['name' => 'TAMIL [TCV]'],
-        ['name' => 'TATAR [TTR]'],
-        ['name' => 'TELUGU [TCW]'],
-        ['name' => 'THAI [THJ]'],
-        ['name' => 'THAI, NORTHEASTERN [TTS]'],
-        ['name' => 'TURKISH [TRK]'],
-        ['name' => 'UKRAINIAN [UKR]'],
-        ['name' => 'URDU [URD]'],
-        ['name' => 'UYGHUR [UIG]'],
-        ['name' => 'UZBEK, NORTHERN [UZB]'],
-        ['name' => 'VIETNAMESE [VIE]'],
-        ['name' => 'YORUBA [YOR]'],
-        ['name' => 'ZHUANG, NORTHERN [CCX]'],
-        ['name' => 'ZULU [ZUU]']
+        [
+            'name' => 'AKAN', 
+            'abbreviation' => 'TWS'
+        ],
+        [
+            'name' => 'AMHARIC', 
+            'abbreviation' => 'AMH'
+        ],
+        [
+            'name' => 'ARABIC, ALGERIAN SPOKEN', 
+            'abbreviation' => 'ARQ'
+        ],
+        [
+            'name' => 'ARABIC, EGYPTIAN SPOKEN', 
+            'abbreviation' => 'ARZ'
+        ],
+        [
+            'name' => 'ARABIC, MESOPOTAMIAN SPOKEN', 
+            'abbreviation' => 'ACM'
+        ],
+        [
+            'name' => 'ARABIC, MOROCCAN SPOKEN', 
+            'abbreviation' => 'ARY'
+        ],
+        [
+            'name' => 'ARABIC, NAJDI SPOKEN', 
+            'abbreviation' => 'ARS'
+        ],
+        [
+            'name' => 'ARABIC, NORTH LEVANTINE SPOKEN', 
+            'abbreviation' => 'APC'
+        ],
+        [
+            'name' => 'ARABIC, SAIDI SPOKEN', 
+            'abbreviation' => 'AEC'
+        ],
+        [
+            'name' => 'ARABIC, SANAANI SPOKEN', 
+            'abbreviation' => 'AYN'
+        ],
+        [
+            'name' => 'ARABIC, SUDANESE SPOKEN', 
+            'abbreviation' => 'APD'
+        ],
+        [
+            'name' => 'ARABIC, TUNISIAN SPOKEN', 
+            'abbreviation' => 'AEB'
+        ],
+        [
+            'name' => 'ASSAMESE', 
+            'abbreviation' => 'ASM'
+        ],
+        [
+            'name' => 'AWADHI', 
+            'abbreviation' => 'AWD'
+        ],
+        [
+            'name' => 'AZERBAIJANI, NORTH', 
+            'abbreviation' => 'AZE'
+        ],
+        [
+            'name' => 'AZERBAIJANI, SOUTH', 
+            'abbreviation' => 'AZB'
+        ],
+        [
+            'name' => 'BELARUSAN', 
+            'abbreviation' => 'RUW'
+        ],
+        [
+            'name' => 'BENGALI', 
+            'abbreviation' => 'BNG'
+        ],
+        [
+            'name' => 'BHOJPURI', 
+            'abbreviation' => 'BHJ'
+        ],
+        [
+            'name' => 'BULGARIAN', 
+            'abbreviation' => 'BLG'
+        ],
+        [
+            'name' => 'BURMESE', 
+            'abbreviation' => 'BMS'
+        ],
+        [
+            'name' => 'CEBUANO', 
+            'abbreviation' => 'CEB'
+        ],
+        [
+            'name' => 'CHHATTISGARHI', 
+            'abbreviation' => 'HNE'
+        ],
+        [
+            'name' => 'CHINESE, GAN', 
+            'abbreviation' => 'KNN'
+        ],
+        [
+            'name' => 'CHINESE, HAKKA', 
+            'abbreviation' => 'HAK'
+        ],
+        [
+            'name' => 'CHINESE, JINYU', 
+            'abbreviation' => 'CJY'
+        ],
+        [
+            'name' => 'CHINESE, MANDARIN', 
+            'abbreviation' => 'CHN'
+        ],
+        [
+            'name' => 'CHINESE, MIN BEI', 
+            'abbreviation' => 'MNP'
+        ],
+        [
+            'name' => 'CHINESE, MIN NAN', 
+            'abbreviation' => 'CFR'
+        ],
+        [
+            'name' => 'CHINESE, WU', 
+            'abbreviation' => 'WUU'
+        ],
+        [
+            'name' => 'CHINESE, XIANG', 
+            'abbreviation' => 'HSN'
+        ],
+        [
+            'name' => 'CHINESE, YUE', 
+            'abbreviation' => 'YUH'
+        ],
+        [
+            'name' => 'CHITTAGONIAN', 
+            'abbreviation' => 'CIT'
+        ],
+        [
+            'name' => 'CZECH', 
+            'abbreviation' => 'CZC'
+        ],
+        [
+            'name' => 'DECCAN', 
+            'abbreviation' => 'DCC'
+        ],
+        [
+            'name' => 'DUTCH', 
+            'abbreviation' => 'DUT'
+        ],
+        [
+            'name' => 'ENGLISH', 
+            'abbreviation' => 'ENG'
+        ],
+        [
+            'name' => 'FARSI, EASTERN', 
+            'abbreviation' => 'PRS'
+        ],
+        [
+            'name' => 'FARSI, WESTERN', 
+            'abbreviation' => 'PES'
+        ],
+        [
+            'name' => 'FRENCH', 
+            'abbreviation' => 'FRN'
+        ],
+        [
+            'name' => 'FULFULDE, NIGERIAN', 
+            'abbreviation' => 'FUV'
+        ],
+        [
+            'name' => 'GERMAN, STANDARD', 
+            'abbreviation' => 'GER'
+        ],
+        [
+            'name' => 'GREEK', 
+            'abbreviation' => 'GRK'
+        ],
+        [
+            'name' => 'GUJARATI', 
+            'abbreviation' => 'GJR'
+        ],
+        [
+            'name' => 'HAITIAN CREOLE FRENCH', 
+            'abbreviation' => 'HAT'
+        ],
+        [
+            'name' => 'HARYANVI', 
+            'abbreviation' => 'BGC'
+        ],
+        [
+            'name' => 'HAUSA', 
+            'abbreviation' => 'HUA'
+        ],
+        [
+            'name' => 'HILIGAYNON', 
+            'abbreviation' => 'HIL'
+        ],
+        [
+            'name' => 'HINDI', 
+            'abbreviation' => 'HND'
+        ],
+        [
+            'name' => 'HUNGARIAN', 
+            'abbreviation' => 'HNG'
+        ],
+        [
+            'name' => 'IGBO', 
+            'abbreviation' => 'IGR'
+        ],
+        [
+            'name' => 'ILOCANO', 
+            'abbreviation' => 'ILO'
+        ],
+        [
+            'name' => 'INDONESIAN', 
+            'abbreviation' => 'INZ'
+        ],
+        [
+            'name' => 'ITALIAN', 
+            'abbreviation' => 'ITN'
+        ],
+        [
+            'name' => 'JAPANESE', 
+            'abbreviation' => 'JPN'
+        ],
+        [
+            'name' => 'JAVANESE', 
+            'abbreviation' => 'JAN'
+        ],
+        [
+            'name' => 'KANNADA', 
+            'abbreviation' => 'KJV'
+        ],
+        [
+            'name' => 'KAZAKH', 
+            'abbreviation' => 'KAZ'
+        ],
+        [
+            'name' => 'KHMER, CENTRAL', 
+            'abbreviation' => 'KMR'
+        ],
+        [
+            'name' => 'KOREAN', 
+            'abbreviation' => 'KKN'
+        ],
+        [
+            'name' => 'KURMANJI', 
+            'abbreviation' => 'KUR'
+        ],
+        [
+            'name' => 'LOMBARD', 
+            'abbreviation' => 'LMO'
+        ],
+        [
+            'name' => 'MADURA', 
+            'abbreviation' => 'MHJ'
+        ],
+        [
+            'name' => 'MAGAHI', 
+            'abbreviation' => 'MQM'
+        ],
+        [
+            'name' => 'MAITHILI', 
+            'abbreviation' => 'MKP'
+        ],
+        [
+            'name' => 'MALAGASY', 
+            'abbreviation' => 'MEX'
+        ],
+        [
+            'name' => 'MALAY', 
+            'abbreviation' => 'MLI'
+        ],
+        [
+            'name' => 'MALAYALAM', 
+            'abbreviation' => 'MJS'
+        ],
+        [
+            'name' => 'MARATHI', 
+            'abbreviation' => 'MRT'
+        ],
+        [
+            'name' => 'MARWARI', 
+            'abbreviation' => 'MKD'
+        ],
+        [
+            'name' => 'NAPOLETANO-CALABRESE', 
+            'abbreviation' => 'NPL'
+        ],
+        [
+            'name' => 'NEPALI', 
+            'abbreviation' => 'NEP'
+        ],
+        [
+            'name' => 'ORIYA', 
+            'abbreviation' => 'ORY'
+        ],
+        [
+            'name' => 'OROMO, WEST-CENTRAL', 
+            'abbreviation' => 'GAZ'
+        ],
+        [
+            'name' => 'PANJABI, EASTERN', 
+            'abbreviation' => 'PNJ'
+        ],
+        [
+            'name' => 'PANJABI, WESTERN', 
+            'abbreviation' => 'PNB'
+        ],
+        [
+            'name' => 'PASHTO, NORTHERN', 
+            'abbreviation' => 'PBU'
+        ],
+        [
+            'name' => 'PASHTO, SOUTHERN', 
+            'abbreviation' => 'PBT'
+        ],
+        [
+            'name' => 'POLISH', 
+            'abbreviation' => 'PQL'
+        ],
+        [
+            'name' => 'PORTUGUESE', 
+            'abbreviation' => 'POR'
+        ],
+        [
+            'name' => 'ROMANIAN', 
+            'abbreviation' => 'RUM'
+        ],
+        [
+            'name' => 'RUSSIAN', 
+            'abbreviation' => 'RUS'
+        ],
+        [
+            'name' => 'RWANDA', 
+            'abbreviation' => 'RUA'
+        ],
+        [
+            'name' => 'SARAIKI', 
+            'abbreviation' => 'SKR'
+        ],
+        [
+            'name' => 'SERBO-CROATIAN', 
+            'abbreviation' => 'SRC'
+        ],
+        [
+            'name' => 'SHONA', 
+            'abbreviation' => 'SHD'
+        ],
+        [
+            'name' => 'SINDHI', 
+            'abbreviation' => 'SND'
+        ],
+        [
+            'name' => 'SINHALA', 
+            'abbreviation' => 'SNH'
+        ],
+        [
+            'name' => 'SOMALI', 
+            'abbreviation' => 'SOM'
+        ],
+        [
+            'name' => 'SPANISH', 
+            'abbreviation' => 'SPN'
+        ],
+        [
+            'name' => 'SUNDA', 
+            'abbreviation' => 'SUO'
+        ],
+        [
+            'name' => 'SWEDISH', 
+            'abbreviation' => 'SWD'
+        ],
+        [
+            'name' => 'TAGALOG', 
+            'abbreviation' => 'TGL'
+        ],
+        [
+            'name' => 'TAMIL', 
+            'abbreviation' => 'TCV'
+        ],
+        [
+            'name' => 'TATAR', 
+            'abbreviation' => 'TTR'
+        ],
+        [
+            'name' => 'TELUGU', 
+            'abbreviation' => 'TCW'
+        ],
+        [
+            'name' => 'THAI', 
+            'abbreviation' => 'THJ'
+        ],
+        [
+            'name' => 'THAI, NORTHEASTERN', 
+            'abbreviation' => 'TTS'
+        ],
+        [
+            'name' => 'TURKISH', 
+            'abbreviation' => 'TRK'
+        ],
+        [
+            'name' => 'UKRAINIAN', 
+            'abbreviation' => 'UKR'
+        ],
+        [
+            'name' => 'URDU', 
+            'abbreviation' => 'URD'
+        ],
+        [
+            'name' => 'UYGHUR', 
+            'abbreviation' => 'UIG'
+        ],
+        [
+            'name' => 'UZBEK, NORTHERN', 
+            'abbreviation' => 'UZB'
+        ],
+        [
+            'name' => 'VIETNAMESE', 
+            'abbreviation' => 'VIE'
+        ],
+        [
+            'name' => 'YORUBA', 
+            'abbreviation' => 'YOR'
+        ],
+        [
+            'name' => 'ZHUANG, NORTHERN', 
+            'abbreviation' => 'CCX'
+        ],
+        [
+            'name' => 'ZULU', 
+            'abbreviation' => 'ZUU'
+            ]
     ];
 }
